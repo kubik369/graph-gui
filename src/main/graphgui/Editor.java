d5909bb86779269df92286350a347db7f05bc68a
Jakub Šimo <jakub.simo.js@gmail.com>
2017-03-19 16:08:05 +0100

    Add checkstyle plugin, fix style in original code

--- src/main/graphgui/Editor.java
+++ src/main/graphgui/Editor.java
@@ -14,7 +14,8 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-/** Trieda, ktorá má obsahovať implementáciu JavaFXovej úlohy A. V
+/**
+ * Trieda, ktorá má obsahovať implementáciu JavaFXovej úlohy A. V
  * tejto triede používajte iba metódy rozhraní Graph, Vertex a Edge,
  * nevolajte priamo žiadne metódy z iných tried balíčka
  * graphgui. Nemeňte hlavičku konštruktora ani public metód edit,
@@ -24,100 +25,97 @@ import javafx.stage.StageStyle;
  */
 public class Editor {
 
-    // POMOCNÁ TRIEDA PRE UKÁŽKOVÝ PRÍKLAD, MEŇTE PODĽA POTREBY
-    class MyStage extends Stage {
-        Vertex v;
-        MyStage(Vertex vertex) {
-            v=vertex;
-            MyStage dialog=this;
-            GridPane pan=new GridPane();
+  // POMOCNÁ TRIEDA PRE UKÁŽKOVÝ PRÍKLAD, MEŇTE PODĽA POTREBY
+  class MyStage extends Stage {
+    Vertex vertex;
 
-            Scene sc = new Scene(pan);
+    MyStage(Vertex vertex) {
+      this.vertex = vertex;
+      MyStage dialog = this;
+      GridPane pan = new GridPane();
 
-            Color c = Color.web(vertex.getColorName());
+      Color c = Color.web(vertex.getColorName());
 
-            TextField rText= new TextField((int)(c.getRed()*255)+"");
-            TextField gText= new TextField((int)(c.getGreen()*255)+"");
-            TextField bText= new TextField((int)(c.getBlue()*255)+"");
-            Label rLabel=new Label("Red (0-255): ");
-            Label gLabel=new Label("Green (0-255): ");
-            Label bLabel=new Label("Blue (0-255): ");
-            Button ok=new Button("OK");
+      final TextField rText = new TextField((int)(c.getRed() * 255) + "");
+      final TextField gText = new TextField((int)(c.getGreen() * 255) + "");
+      final TextField bText = new TextField((int)(c.getBlue() * 255) + "");
+      final Label rLabel = new Label("Red (0-255): ");
+      final Label gLabel = new Label("Green (0-255): ");
+      final Label bLabel = new Label("Blue (0-255): ");
+      Button ok = new Button("OK");
 
-            ok.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    try {
-                        double r=(double)Integer.parseInt(rText.getText());
-                        double g=(double)Integer.parseInt(gText.getText());
-                        double b=(double)Integer.parseInt(bText.getText());
-                        if (r>=0 && g>=0 && b>=0 && r<256 && g<256 && b<256) {
-                            vertex.setColorName(new Color(r/255,g/255,b/255,1).toString());
-                            dialog.close();
-                        }
-                    }
-                    catch (Exception e) {
-			System.err.println("Problem s nastavenim farby");
-                    }
-                }
-            });
-
-            pan.getColumnConstraints().add(new ColumnConstraints(100));
-            pan.setRowIndex(rLabel,0);
-            pan.setColumnIndex(rLabel,0);
-            pan.setRowIndex(rText,0);
-            pan.setColumnIndex(rText,1);
-            pan.setRowIndex(gLabel,1);
-            pan.setColumnIndex(gLabel,0);
-            pan.setRowIndex(gText,1);
-            pan.setColumnIndex(gText,1);
-            pan.setRowIndex(bLabel,2);
-            pan.setColumnIndex(bLabel,0);
-            pan.setRowIndex(bText,2);
-            pan.setColumnIndex(bText,1);
-            pan.setRowIndex(ok,3);
-            pan.setColumnIndex(ok,1);
-
-            pan.getChildren().addAll(rLabel,rText,gLabel,gText,bLabel,bText, ok);
-
-            this.setScene(sc);
+      ok.setOnAction(new EventHandler<ActionEvent>() {
+        @Override
+        public void handle(ActionEvent event) {
+          try {
+            double r = (double)Integer.parseInt(rText.getText());
+            double g = (double)Integer.parseInt(gText.getText());
+            double b = (double)Integer.parseInt(bText.getText());
+            if (r >= 0 && g >= 0 && b >= 0 && r < 256 && g < 256 && b < 256) {
+              vertex.setColorName(new Color(r / 255, g / 255, b / 255, 1).toString());
+              dialog.close();
+            }
+          } catch (Exception e) {
+            System.err.println("Problem s nastavenim farby");
+          }
         }
-    }
+      });
 
-    // PREMENNÉ TRIEDY, UPRAVTE SI PODĽA POTREBY
-    private Graph graph;
+      pan.getColumnConstraints().add(new ColumnConstraints(100));
+      pan.setRowIndex(rLabel, 0);
+      pan.setColumnIndex(rLabel, 0);
+      pan.setRowIndex(rText, 0);
+      pan.setColumnIndex(rText, 1);
+      pan.setRowIndex(gLabel, 1);
+      pan.setColumnIndex(gLabel, 0);
+      pan.setRowIndex(gText, 1);
+      pan.setColumnIndex(gText, 1);
+      pan.setRowIndex(bLabel, 2);
+      pan.setColumnIndex(bLabel, 0);
+      pan.setRowIndex(bText, 2);
+      pan.setColumnIndex(bText, 1);
+      pan.setRowIndex(ok, 3);
+      pan.setColumnIndex(ok, 1);
 
-    // KONŠTRUKTOR: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
-    /** Konštruktor triedy, ktorý dostane graf. */
-    public Editor(Graph graph) {
-        this.graph = graph;
-    }
+      pan.getChildren().addAll(rLabel, rText, gLabel, gText, bLabel, bText, ok);
 
-    // METÓDA editVertex: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
-    /**
-     * Akcia, ktorá sa má vykonať v prípade, že je vybratý nejaký vrchol
-     * @param vertex vybraný vrchol
-     */
-    public void editVertex(Vertex vertex) {
-        MyStage dialog=new MyStage(vertex);
-        dialog.initStyle(StageStyle.UTILITY);
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.show();
+      Scene sc = new Scene(pan);
+      this.setScene(sc);
     }
+  }
 
-    // METÓDA editEdge: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
-    /**
-     * Akcia, ktorá sa má vykonať v prípade, že je vybratá nejaká hrana
-     * @param edge vybraná hrana
-     */
-    public void editEdge(Edge edge) {
-    }
+  // PREMENNÉ TRIEDY, UPRAVTE SI PODĽA POTREBY
+  private Graph graph;
 
-    // METÓDA edit: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
-    /**
-     * Akcia, ktorá sa má vykonať v prípade, že nie je vybratý žiadny vrchol
-     */
-    public void edit() {
-    }
+  // KONŠTRUKTOR: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
+  /** Konštruktor triedy, ktorý dostane graf. */
+  public Editor(Graph graph) {
+    this.graph = graph;
+  }
+
+  // METÓDA editVertex: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
+  /**
+   * Akcia, ktorá sa má vykonať v prípade, že je vybratý nejaký vrchol.
+   * @param vertex vybraný vrchol
+   */
+  public void editVertex(Vertex vertex) {
+    MyStage dialog = new MyStage(vertex);
+    dialog.initStyle(StageStyle.UTILITY);
+    dialog.initModality(Modality.APPLICATION_MODAL);
+    dialog.show();
+  }
+
+  // METÓDA editEdge: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
+  /**
+   * Akcia, ktorá sa má vykonať v prípade, že je vybratá nejaká hrana.
+   * @param edge vybraná hrana
+   */
+  public void editEdge(Edge edge) {
+  }
+
+  // METÓDA edit: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
+  /** Akcia, ktorá sa má vykonať v prípade, že nie je vybratý žiadny vrchol. */
+  public void edit() {
+  }
 
 }
--- src/main/graphgui/GraphAlgorithm.java
+++ src/main/graphgui/GraphAlgorithm.java
@@ -1,6 +1,7 @@
 package graphgui;
 
-/** Trieda, ktorá má obsahovať implementáciu požadovaného grafového
+/**
+ * Trieda, ktorá má obsahovať implementáciu požadovaného grafového
  * algoritmu z úlohy B. V tejto triede používajte iba metódy z
  * rozhraní Graph, Edge a Vertex. Nevolajte priamo žiadne metódy
  * z iných tried balíčka graphgui. Nemeňte hlavičku
@@ -10,76 +11,76 @@ package graphgui;
  */
 public class GraphAlgorithm {
 
-    //PREMENNÉ TRIEDY, UPRAVTE SI PODĽA POTREBY
+  //PREMENNÉ TRIEDY, UPRAVTE SI PODĽA POTREBY
 
-    /** Samotný graf, na ktorom spúšťame algoritmus */
-    private Graph graph;
-    /** Vybraný vrchol, ktorý je parametrom algoritmu,
-     * môže byť aj null. */
-    private Vertex selectedVertex;
-    /** Vybraný vrchol, ktorý je parametrom algoritmu,
-     * môže byť aj null. */
-    private Edge selectedEdge;
+  /** Samotný graf, na ktorom spúšťame algoritmus. */
+  private Graph graph;
 
-    // KONŠTRUKTOR: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
-    /** Konštruktor triedy, ktorý dostane graf, vybraný vrchol a hranu
-     * (tento vrchol a hrana môžu byť aj null). */
-    public GraphAlgorithm(Graph graph,
-                          Vertex selectedVertex, Edge selectedEdge) {
-        // uloz vstupne udaje
-        this.graph = graph;
-        this.selectedVertex = selectedVertex;
-        this.selectedEdge = selectedEdge;
-    }
+  /** Vybraný vrchol, ktorý je parametrom algoritmu, môže byť aj null. */
+  private Vertex selectedVertex;
+
+  /** Vybraný vrchol, ktorý je parametrom algoritmu, môže byť aj null. */
+  private Edge selectedEdge;
+
+  // KONŠTRUKTOR: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
+  /**
+   * Konštruktor triedy, ktorý dostane graf, vybraný vrchol a hranu
+   * (tento vrchol a hrana môžu byť aj null).
+   */
+  public GraphAlgorithm(Graph graph, Vertex selectedVertex, Edge selectedEdge) {
+    // uloz vstupne udaje
+    this.graph = graph;
+    this.selectedVertex = selectedVertex;
+    this.selectedEdge = selectedEdge;
+  }
 
 
-    // METÓDA performAlgorithm: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
-    /** Metóda, ktorá spustí výpočet a upraví graf podľa špecifikácie
-     * v zadaní úlohy. Vráti výsledok v textovej forme sformátovaný podľa
-     * pokynov v zadaní.
-     */
-    public String performAlgorithm() {
-        // ukazkovy jednoduchy algoritmus:
-        // ak selectedVertex nie je null, spustíme z neho
-        // prehľadávanie do hĺbky, pričom počítame navštívené vrcholy
-        // a nafarbíme ich na oranžovo
+  // METÓDA performAlgorithm: NEMEŇTE HLAVIČKU, TELO UPRAVTE PODĽA POTREBY
+  /**
+   * Metóda, ktorá spustí výpočet a upraví graf podľa špecifikácie
+   * v zadaní úlohy. Vráti výsledok v textovej forme sformátovaný podľa
+   * pokynov v zadaní.
+   */
+  public String performAlgorithm() {
+    // ukazkovy jednoduchy algoritmus:
+    // ak selectedVertex nie je null, spustíme z neho
+    // prehľadávanie do hĺbky, pričom počítame navštívené vrcholy
+    // a nafarbíme ich na oranžovo
+    int visitedVertices = 0;
 
-        int visitedVertices = 0;
+    // ak bol nejaky vrchol vybrany
+    if (selectedVertex != null) {
+      // vytvor pole visited, vypln false
+      int n = graph.getNumberOfVertices();
+      boolean[] visited = new boolean[n];
+      for (int i = 0; i < n; i++) {
+        visited[i] = false;
+      }
 
-        // ak bol nejaky vrchol vybrany
-        if (selectedVertex != null) {
+      // rekurzivne prehladavanie
+      search(visited, selectedVertex);
 
-            // vytvor pole visited, vypln false
-            int n = graph.getNumberOfVertices();
-            boolean[] visited = new boolean[n];
-            for(int i=0; i<n; i++) {
-                visited[i] = false;
-            }
-
-            // rekurzivne prehladavanie
-            search(visited, selectedVertex);
-
-            // prefarbi a spocitaj navstivene vrcholy
-            for(int i=0; i<n; i++) {
-                if(visited[i]) {
-                    graph.getVertex(i).setColorName("orange");
-                    visitedVertices++;
-                }
-            }
+      // prefarbi a spocitaj navstivene vrcholy
+      for (int i = 0; i < n; i++) {
+        if (visited[i]) {
+          graph.getVertex(i).setColorName("orange");
+          visitedVertices++;
         }
-        // v tejto ukazke vratime pocet navstivenych vrcholov
-        return "Pocet navstivenych vrcholov: " + visitedVertices;
+      }
     }
+    // v tejto ukazke vratime pocet navstivenych vrcholov
+    return "Pocet navstivenych vrcholov: " + visitedVertices;
+  }
 
-    // POMOCNÉ METÓDY A TRIEDY, MEŇTE A PRIDÁVAJTE PODĽA POTREBY
-    // v ukážkovom príklade metóda search rekurzívne prehľadáva graf do hĺbky
-    private void search(boolean[] visited, Vertex vertex) {
-        visited[vertex.getId()] = true;
-        for(Vertex neighbour : vertex.adjVertices()) {
-            if(!visited[neighbour.getId()]) {
-                visited[neighbour.getId()] = true;
-                search(visited, neighbour);
-            }
-        }
+  // POMOCNÉ METÓDY A TRIEDY, MEŇTE A PRIDÁVAJTE PODĽA POTREBY
+  // v ukážkovom príklade metóda search rekurzívne prehľadáva graf do hĺbky
+  private void search(boolean[] visited, Vertex vertex) {
+    visited[vertex.getId()] = true;
+    for (Vertex neighbour : vertex.adjVertices()) {
+      if (!visited[neighbour.getId()]) {
+        visited[neighbour.getId()] = true;
+        search(visited, neighbour);
+      }
     }
+  }
 }
